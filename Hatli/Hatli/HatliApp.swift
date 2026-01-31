28927a711271c478a1f7d08e8452ced224e42cd7
aya <ayamashaly363@gmail.com>
2026-01-31 20:52:19 +0200

    initial project

---
+++ Hatli/Hatli/HatliApp.swift
@@ -0,0 +1,17 @@
+//
+//  HatliApp.swift
+//  Hatli
+//
+//  Created by Aya Mashaly on 31/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct HatliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
